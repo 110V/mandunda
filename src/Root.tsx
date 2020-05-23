44f717d2f13601297c64c0d704c37215daab79c3
110V <sgb1616@gmail.com>
2020-05-24 01:23:39 +0900

    해쉬 라우터로 변경

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import GameScreen from "./components/GameScreen";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import GamePlayer from "./GamePlayer";
 
 const Root = () => {
     return(
         <div>
-            <BrowserRouter>
+            <HashRouter>
                 <Route exact path="/" component={App} />
                 <Route path="/player/:id" component={GamePlayer} />
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
